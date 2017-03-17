7780b2e7e4ebda88c5134c049078cc7f130e63d4
Crusader <crusader@masterofcode.com>
2017-03-17 10:09:56 +0200

    Update from server.

--- frontend/templates/inquire/candidate.php
+++ frontend/templates/inquire/candidate.php
@@ -4,10 +4,12 @@
  * @var String $userRole
  * @var Array $inquiries
  */
+$sniff_host = $_SERVER["HTTP_HOST"]; // returns what is after https:// and before first slash 
+
 
 ?>
 <div class="page-header">
-    <h1><?php echo __("Inquire list", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></h1>
+    <h1><?php echo __("Inquiry list", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></h1>
 </div>
 <div class="page-content">
     <?php if($inquiries): ?>
@@ -16,6 +18,7 @@
         <tr>
             <th>#</th>
             <th><?php echo sprintf(__('%s title', \MissionNext\lib\Constants::TEXT_DOMAIN), ucfirst(getCustomTranslation(\MissionNext\lib\Constants::ROLE_JOB))) ?></th>
+            <th><?php echo __('Second Title', \MissionNext\lib\Constants::TEXT_DOMAIN) ?></th>
             <th><?php echo ucfirst(getCustomTranslation(\MissionNext\lib\Constants::ROLE_ORGANIZATION)) ?></th>
             <th><?php echo __('Favorite', \MissionNext\lib\Constants::TEXT_DOMAIN) ?></th>
             <th><?php echo __('Actions', \MissionNext\lib\Constants::TEXT_DOMAIN)?></th>
@@ -26,8 +29,9 @@
         <?php foreach($inquiries as $key => $inquire): ?>
         <tr data-id="<?php echo $inquire['id'] ?>">
             <td class="id"><?php echo $key+1 ?></td>
-            <td class="name"><a href="/job/<?php echo $inquire['id'] ?>"><?php echo $inquire['name'] ?></a></td>
-            <td class="name"><a href="/organization/<?php echo $inquire['organization']['id'] ?>"><?php echo $inquire['organization']['profileData']['organization_name'] ?></a></td>
+            <td class="name"><a target="_blank" href="/job/<?php echo $inquire['id'] ?>"><?php echo $inquire['name'] ?></a> </td>
+            <td class="name"><?php echo $inquire['profileData']['second_title'] ?> </td>
+            <td class="name"><a target="_blank" href="/organization/<?php echo $inquire['organization']['id'] ?>"><?php echo $inquire['organization']['profileData']['organization_name'] ?></a></td>
             <td class="favorite">
                 <div class="favorite-block <?php echo isset($inquire['favorite'])?'favorite':'not-favorite' ?>"></div>
             </td>

bf95f16ac5734557b7d1ba22063414852cd1901d
Crusader <crusader@masterofcode.com>
2017-05-11 11:18:06 +0300

    Bug #60152. Have changed order of the folders.

--- frontend/templates/common/_agency_table.php
+++ frontend/templates/common/_agency_table.php
@@ -19,8 +19,6 @@ $foldersApi = \MissionNext\lib\core\Context::getInstance()->getApiManager()->get
 $default_folder_id = \MissionNext\lib\SiteConfig::getDefaultFolder($role);
 $default_folder = '';
 
-uasort($foldersApi, 'sortFolders');
-
 $folders = array();
 
 foreach($foldersApi as $folderApi){
@@ -70,10 +68,6 @@ if($affiliate){
 }
 
 //FAVORITES
-
-function sortFolders($a, $b){
-    return $a['id'] < $b['id']? -1: 1;
-}
 
 function getProfileField($item, $symbol_key){
 
--- frontend/templates/common/_job_table.php
+++ frontend/templates/common/_job_table.php
@@ -20,8 +20,6 @@ $foldersApi = \MissionNext\lib\core\Context::getInstance()->getApiManager()->get
 $default_folder_id = \MissionNext\lib\SiteConfig::getDefaultFolder($role);
 $default_folder = '';
 
-uasort($foldersApi, 'sortFolders');
-
 $folders = array();
 
 foreach($foldersApi as $folderApi){
@@ -59,10 +57,6 @@ foreach($items as $item){
 $matching = isset($items[0]['matching_percentage']);
 
 //FAVORITES
-
-function sortFolders($a, $b){
-    return $a['id'] < $b['id']? -1: 1;
-}
 
 function getProfileField($item, $symbol_key){
 

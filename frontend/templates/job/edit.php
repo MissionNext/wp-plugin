c6055b564a950fde0f9ffc3aa11d6ac0ae7c83bd
sirCrusader <sercrusader@gmail.com>
2017-08-04 09:01:05 +0300

    Update from Nelson.

--- frontend/templates/job/edit.php
+++ frontend/templates/job/edit.php
@@ -4,7 +4,23 @@
 <div class="page-content job-form">
 
     <form role="form" action="<?php echo $_SERVER['REQUEST_URI']?>" method="POST" class="form-horizontal" enctype="multipart/form-data">
-
+<?php 
+$url_string = $_SERVER['REQUEST_URI']; 
+$this_jobid = intval(preg_replace('/[^0-9]+/', '', $url_string), 10); // echo $this_jobid; // extracts intgers from a string 
+require_once("connect.inc.php");
+	$sql_pri = "SELECT name FROM jobs WHERE id = $this_jobid";
+	$res_pri = pg_query($db_link,$sql_pri) or die("\$sql_pri query failed: <br>$sql_pri");
+		if ($row_pri = pg_fetch_array($res_pri)) {
+			$first_title = $row_pri[0];
+		}
+	$sql_sec = "SELECT value FROM job_profile WHERE job_id = $this_jobid AND field_id = 2";
+	$res_sec = pg_query($db_link,$sql_sec) or die("\$sql_sec query failed: <br>$sql_sec");
+		if ($row_sec = pg_fetch_array($res_sec)) {
+			$second_title = $row_sec[0];
+		}
+pg_close($db_link);
+print ("<br>EDITING JOB: $first_title&#151;$second_title"); 
+?>
         <?php \MissionNext\lib\core\Context::getInstance()->getTemplateService()->render("_form", array('form' => $form)) ?>
 
         <div class="control-buttons">
--- frontend/templates/job/index.php
+++ frontend/templates/job/index.php
@@ -15,23 +15,26 @@ $sniff_host = $_SERVER["HTTP_HOST"]; // returns what is after http:// and before
 // echo "<br>$sniff_host";
 if (preg_match("/explorenext/",$sniff_host))   { $this_app = 3; }
 elseif (preg_match("/teachnext/",$sniff_host)) { $this_app = 6; }
-// echo "<br>\$this_app = $this_app";
+$number_jobs = count($jobs);
+// echo "<br>\$this_app = $this_app; \$number_jobs = $number_jobs";
 ?>
 
 <div class="page-header">
     <h1><?php echo ucfirst(getCustomTranslation(\MissionNext\lib\Constants::ROLE_JOB_PLURAL)) ?></h1>
 </div>
 <div class="page-content">
-    <?php if($jobs): ?> 
-    
-    <table class="table table-bordered">
+    <?php if($jobs): 
+    if ($number_jobs > 9): ?> 
+    <a class="btn btn-success" href="/job/new"><?php echo __("New Job", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></a><br><br>
+     <?php endif; ?> 
+     <table class="table table-bordered">
         <thead>
             <tr>
-                <td class="expiration"><strong><?php echo __("Expires", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
+                <td class="expiration" width="80"><strong><?php echo __("Expires", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
                 <td class="name"><strong><?php echo __("Category", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
                 <td class="alt_title"><strong><?php echo __("Job Title", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
                 <td class="country"><strong><?php echo __("Location", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
-                <td class="actions" style="text-align:center"><strong><?php echo __("Actions", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
+                <td class="actions" style="text-align:center" width="320"><strong><?php echo __("Actions", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></strong></td>
             </tr>
         </thead>
         <tbody>
@@ -39,11 +42,11 @@ elseif (preg_match("/teachnext/",$sniff_host)) { $this_app = 6; }
         <?php if($this_app == $job['app_id']): ?>
         <?php if ($date_today > $job['profileData']['listing_expiration']) { $font = "red"; $warn = "Yes"; } else { $font = "black"; } ?>
             <tr>
-                <td class="expiration"><font color='<?php echo $font ?>'><?php echo $job['profileData']['listing_expiration'] ?></font></td>
+                <td class="expiration" width="80"><font color='<?php echo $font ?>'><?php echo $job['profileData']['listing_expiration'] ?></font></td>
                 <td class="name"><a href="/job/<?php echo $job['id'] ?>" target="_blank"><?php echo $job['name'] ?></a> </td>
                 <td class="alt_title"><?php echo $job['profileData']['second_title'] ?></td>
                 <td class="country"><?php echo $job['profileData']['country'] ?></td>
-                <td class="actions">
+                <td class="actions" width="320">
                     <a class="btn btn-primary" href="/job/matches/candidate/<?php echo $job['id'] ?>"><?php echo __("Matches", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></a>
                     <a class="btn btn-warning" href="/job/<?php echo $job['id'] ?>/edit"><?php echo __("Edit", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></a>
                     <a class="btn btn-danger" href="/job/<?php echo $job['id'] ?>/delete"><?php echo __("Delete", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></a>
@@ -66,5 +69,5 @@ elseif (preg_match("/teachnext/",$sniff_host)) { $this_app = 6; }
     <?php endif; 
     if ($warn == "Yes") { echo "<font color='red'>NOTICE: One or more jobs has expired. Edit / Save job spec to extend the expiration date for another 6 months.</font><br>"; }      
     ?>
-    <a class="btn btn-success" href="/job/new"><?php echo __("New", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></a>
+    <a class="btn btn-success" href="/job/new"><?php echo __("New Job", \MissionNext\lib\Constants::TEXT_DOMAIN) ?></a>
 </div>

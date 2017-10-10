cf13f1443ad60059c9b1c6ad3953193eb83a49ae
Crusader <crusader@masterofcode.com>
2017-10-10 12:07:40 +0300

    Feature #63703. Changed default value logic for Input fields.

--- lib/form/fields/InputField.php
+++ lib/form/fields/InputField.php
@@ -8,14 +8,11 @@ class InputField extends BaseField {
 
     public function printField ($options = array())
     {
-        $default = ($this->default && $this->field['type'] != 'password')?$this->default:null;
-        if (is_null($this->default) && !empty($this->field['default_value'])) {
-            $default = $this->field['default_value'];
-        }
+        $default = ($this->default && $this->field['type'] != 'password') ? $this->default : null;
 
-        $placeholder = is_array($this->field['default_value'])?current($this->field['default_value']):$this->field['default_value'];
+        $placeholder = is_array($this->field['default_value']) ? current($this->field['default_value']) : $this->field['default_value'];
 
-        if(is_array($default)){
+        if (is_array($default)) {
             $default = current($default);
         }
 
@@ -37,10 +34,8 @@ class InputField extends BaseField {
         }
         $key = $this->name;
         $id = $this->id;
-        if ($default) { $placeholder = $default; } // Added by Nelson Nov 13 2015
+
 		$field = "<input $optionsString id=\"$id\" name=\"$key\" type=\"$type\" data-type=\"$type\" value=\"$default\" placeholder='$placeholder'/>";
-		
-       	// $field = "<input $optionsString id=\"$id\" name=\"$key\" type=\"$type\" data-type=\"$type\" value=\"$placeholder\" placeholder='$placeholder'/>";
 
         return $field;
 

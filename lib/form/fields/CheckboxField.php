dc9cd7b74f426abfae04ca1dbf7a0bf013d2da91
Vladyslav Yashchuk <vladyslav.yashchuk@masterofcode.com>
2023-09-18 18:20:10 +0300

    checkbox field fix

--- lib/form/fields/CheckboxField.php
+++ lib/form/fields/CheckboxField.php
@@ -26,10 +26,12 @@ class CheckboxField extends BaseField {
         $key = $this->id;
         $name = $this->name;
 
-        if(isset($this->constraints[0]) && $this->constraints[0] == 'required')
+        if(isset($this->constraints[0]) && $this->constraints[0] == 'required') {
             $field = "<input $optionsString name=\"$name\" id=\"$key\" type=\"checkbox\" value=\"1\" $checked />";
-        else
-            $field = "<input name=\"$name\" id=\"$key\" type=\"hidden\" value=\"0\"><input $optionsString name=\"$name\" id=\"$key\" type=\"checkbox\" value=\"1\" $checked />";
+        } else {
+            $hiddenKey = $key."_hidden";
+            $field = "<input name=\"$name\" id=\"$hiddenKey\" type=\"hidden\" value=\"0\"><input $optionsString name=\"$name\" id=\"$key\" type=\"checkbox\" value=\"1\" $checked />";
+        }
 
         return $field;
 

d041927927279410da8e4b1dbb7b6fa2d5e75095
Vladyslav Yashchuk <vladyslav.yashchuk@masterofcode.com>
2024-09-17 11:51:10 +0300

    Added condition to show Thank you page to candidates only

--- frontend/controllers/profileController.php
+++ frontend/controllers/profileController.php
@@ -44,7 +44,7 @@ class profileController extends AbstractLayoutController {
                 if (!$meta_value) {
                     update_user_meta($wp_user_id, 'thank_you_page_'.$site_id, 1);
                     $thank_you_page = get_page_by_path( 'mn-thank-you-page', OBJECT, 'page' );
-                    if ($thank_you_page) {
+                    if ($thank_you_page && $this->userRole == Constants::ROLE_CANDIDATE) {
                         $this->redirect('/mn-thank-you-page');
                     } else {
                         $this->redirect($_SERVER['REQUEST_URI']);
